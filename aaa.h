9e2e2947f466c063b8e99dbdebe305bde0d16f85
Greg <145311515+GregWaters@users.noreply.github.com>
2024-06-27 11:08:18 -0500

    Update aaa.h

--- aaa.h
+++ aaa.h
@@ -1,15 +1,13 @@
+#include <stdio.h>
+
 #define a int
 #define aa main
 #define aaa (
 #define aaaa )
-#define aaaaa {
-#define aaaaaa while
-#define aaaaaaa 1
-#define aaaaaaaa printf
-#define aaaaaaaaa "%s"
-#define aaaaaaaaaa ,
-#define aaaaaaaaaaa "a"
-#define aaaaaaaaaaaa ;
-#define aaaaaaaaaaaaa return
-#define aaaaaaaaaaaaaa 0
-#define aaaaaaaaaaaaaaa }
+#define aaaaa while
+#define aaaaaa 1
+#define aaaaaaa putchar
+#define aaaaaaaa {
+#define aaaaaaaaa }
+#define aaaaaaaaaa 'a'
+#define aaaaaaaaaaa ;
