fde3037deb702674a5338bea15a187d7ca07f362
Greg <145311515+GregWaters@users.noreply.github.com>
2024-06-28 13:07:29 -0500

    Update aaa.asm

--- aaa.asm
+++ aaa.asm
@@ -1,17 +1,11 @@
-; nasm -f elf64 -o aaa.o aaa.asm && ld -o aaa aaa.o && ./aaa
+; "Yeah I write code"
+    db 0xB8
+    dd 0q1
+    dd 0xC289 << (0x2 << 0q2 << 0b1) | 0x4288C789
+    dw 0x348D & -1
+    db 0x5 | 0b00100000 ^ ~0xFF
+    dd because_im
+    dd -9.76233889780912372017480E+36
 
-SECTION .data
-A: DB 'a'
-
-SECTION .text
-GLOBAL _start
-
-_start:
-        MOV RAX, 1
-        MOV RDI, RAX
-        MOV RDX, RAX
-        MOV RSI, A
-
-LOOP:
-        SYSCALL
-        JMP LOOP
+because_im:
+    db ~0x9E
